7c76ea73173bd877f2198c5615ce1dbaaf01cb73
monspi <git@monspi.cn>
2024-01-28 12:10:26 +0800

    fix boss battle

---
+++ Assets/SpaceCartControllerBoss.cs
@@ -0,0 +1,95 @@
+
+using UnityEngine;
+
+public class SpaceCartControllerBoss : MonoBehaviour
+{
+    // A reference to the Rigidbody2D component
+    public Boss boss;
+    // The speed of the player movement
+    public float speed = 5f;
+    public float moveRange = 1.5f;
+
+    public float minX = -10f;
+    public float maxX = 10f;
+    public float minY = -10f;
+    public float maxY = 10f;
+
+    private Rigidbody2D rb;
+    private float moveTime;
+    private float nextMoveTime;
+    private Vector3 originLocation;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the Rigidbody2D component from the game object
+        rb = GetComponent<Rigidbody2D>();
+        moveTime = 0f;
+        nextMoveTime = 0.3f;
+        originLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boss.health > 0)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            // Create a vector for the movement direction
+            Vector2 direction = new Vector2(horizontal, vertical);
+
+            // Move the rigidbody in the direction with the speed
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            moveTime += Time.deltaTime;
+            if (moveTime >= nextMoveTime)
+            {
+                RandomizeMoveDirection();
+                moveTime = 0;
+                nextMoveTime = Random.Range(0.4f, 1.2f);
+            }
+            AutoMove();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.GetComponent<Bullet>() != null)
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    void RandomizeMoveDirection()
+    {
+        moveDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+
+    void AutoMove()
+    {
+        if (transform.position.x - originLocation.x >= moveRange)
+        {
+            moveDirection.x = -Mathf.Abs(moveDirection.x);
+        }
+        else if (transform.position.x - originLocation.x <= - moveRange)
+        {
+            moveDirection.x = Mathf.Abs(moveDirection.x);
+        }
+
+        if (transform.position.y - originLocation.y >= moveRange)
+        {
+            moveDirection.y = -Mathf.Abs(moveDirection.y);
+        }
+        else if (transform.position.y - originLocation.y <= - moveRange)
+        {
+            moveDirection.y = Mathf.Abs(moveDirection.y);
+        } 
+
+        rb.velocity = moveDirection * speed * 0.6f;
+    }
+}
